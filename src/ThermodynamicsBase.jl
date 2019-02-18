68447df3d38979c385171f8dece7d3eee1fcea89
doomphoenix-qxz <richardlfitzhugh@gmail.com>
2019-02-18 12:12:45 -0700

    Exports in ThermodynamicsBase

--- src/ThermodynamicsBase.jl
+++ src/ThermodynamicsBase.jl
@@ -3,6 +3,9 @@ module ThermodynamicsBase
 include("abstracts.jl")
 include("constants.jl")
 
-export AbstractPhaseState, AbstractSubstance, AbstractThermoBackend, 
+export AbstractPhaseState, AbstractSubstance, AbstractThermoBackend,
+AbstractMixture, AbstractEquationOfState, AbstractMixingRules, AbstractProperty
+
+export R, thermocalc, thermocalc!
 
 end
