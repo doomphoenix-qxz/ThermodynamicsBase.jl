00d820695c2c87bbd510a96c081cc096337fd328
doomphoenix-qxz <richardlfitzhugh@gmail.com>
2019-02-19 16:12:11 -0700

    Add type AbstractActivityCoefficientModel

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -4,6 +4,7 @@ abstract type AbstractMixture end
 abstract type AbstractThermoBackend end
 abstract type AbstractEquationOfState<:AbstractThermoBackend end
 abstract type AbstractMixingRules<:AbstractThermoBackend end
+abstract type AbstractActivityCoefficientModel <: AbstractThermoBackend end 
 abstract type AbstractProperty end
 
 @enum ThermoProp pressure=1 volume=2 temperature=3 entropy=4 internalenergy=5 enthalpy=6 helmholtzenergy=7  gibbsenergy=8 heatcapacity=9 vaporpressure=10 chemicalpotential=11 fugacity=12 fugacitycoefficient=13 activity=14 activitycoefficient=15
